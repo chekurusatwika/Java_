14cb120f74813d8ddac2282394c5fddf02b4c3e6
Satwika <chekurusatwika@gmail.com>
2024-08-20 17:40:52 +0530

    converting unicode into character

---
+++ javaPrograming/day1workingwithprimitives/Exercise/Unicode.java
@@ -0,0 +1,8 @@
+package com.javaPrograming.day1workingwithprimitives.Exercise;
+
+public class Unicode {
+    public static void main(String[] args) {
+        char unicodeChar = '\u0041';
+        System.out.println("The unicode character is : " + unicodeChar);
+    }
+}
