9aeb64cb41ed7cc714310b4731f01617a818b13b
Satwika <chekurusatwika@gmail.com>
2024-08-20 18:16:48 +0530

    keywords cannot be used as variable names example

---
+++ javaPrograming/day1workingwithprimitives/Exercise/PackageEmployee.java
@@ -0,0 +1,10 @@
+package com.javaPrograming.day1workingwithprimitives.Exercise;
+
+public class PackageEmployee {
+    public static void main(String[] args) {
+        int age = 26;
+        String name = "hello kitty";
+         int pack = 29; // here i have used package as variable name which cannot be taken so i have changed to pack.keywords cannot be used as variable names
+        System.out.println(age+" "+ name+" "+pack );
+    }
+}
