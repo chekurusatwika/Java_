c8e7232101a75d9d045a68d413003d2c66b8937d
Satwika <chekurusatwika@gmail.com>
2024-08-20 17:29:41 +0530

    converting the character into ascii value

---
+++ javaPrograming/day1workingwithprimitives/Exercise/AsciiExample.java
@@ -0,0 +1,10 @@
+package com.javaPrograming.day1workingwithprimitives.Exercise;
+
+public class AsciiExample {
+    public static void main(String[] args) {
+        char letter = 'A';
+         int asciiValue;
+        asciiValue = (int)letter;
+        System.out.println("the ascii value of A is : " + asciiValue);
+    }
+}
