2db11e121e9a496a5b87dd33f76e5401d05dfa23
Satwika <chekurusatwika@gmail.com>
2024-08-20 12:52:17 +0530

    Assign all types of primitives values of employee in single code

---
+++ javaPrograming/day1workingwithprimitives/Exercise/Employee.java
@@ -0,0 +1,40 @@
+package com.javaPrograming.day1workingwithprimitives.Exercise;
+
+public class Employee {
+    public static void main(String[] args) {
+        byte age;
+        age = 52;
+        System.out.println("Age in the byte range : " + age);
+
+        short empNo;
+        empNo = 458;
+        System.out.println("empNo in short range : " + empNo);
+
+        int salary;
+        salary = 20000;
+        System.out.println("salary in int range : " + salary);
+
+        long monthlySalary;
+        monthlySalary = 250000000;
+        System.out.println("Monthly Salary in long range: " + monthlySalary);
+
+        float pf;
+        pf = 5000.01f;
+        System.out.println("Provident fund in float range: " + pf);
+
+        double annualSalary;
+        annualSalary = 500000000d;
+        System.out.println("AnnualSalary in double range: " + annualSalary);
+
+        char alphabet;
+        alphabet = 'S';
+        System.out.println("Alphabet is : " + alphabet);
+
+        boolean isEligible;
+        isEligible = true;
+        System.out.println("Eligibility status : " + true);
+
+
+
+    }
+}
