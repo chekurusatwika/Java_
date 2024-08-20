5ea032b42628afebbc43711897741d8067a6126c
Satwika <chekurusatwika@gmail.com>
2024-08-20 18:07:27 +0530

    all types of literals

---
+++ javaPrograming/day1workingwithprimitives/Exercise/LiteralExample.java
@@ -0,0 +1,24 @@
+package com.javaPrograming.day1workingwithprimitives.Exercise;
+
+public class LiteralExample {
+    public static void main(String[] args) {
+        int age;
+        age = 50;
+        System.out.println("age : " + age);
+
+        float salary;
+        salary = 500000.45f;
+        System.out.println("salary : " + salary);
+
+        char grade;
+        grade = 'A';
+        System.out.println("grade : " + grade);
+
+        boolean isFinal;
+        isFinal = true;
+        System.out.println("isfinal : " + true);
+
+
+
+    }
+}
