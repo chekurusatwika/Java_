af6690e50d34986d71e9e7dafc352fc76047811b
Satwika <chekurusatwika@gmail.com>
2024-09-15 23:07:45 +0530

    do while loop implementation

--- javaPrograming/day2conditionalStatements/task/DoWhile.java
+++ javaPrograming/day2conditionalStatements/task/DoWhile.java
@@ -1,4 +1,11 @@
 package com.javaPrograming.day2conditionalStatements.task;
 
 public class DoWhile {
+    public static void main(String[] args) {
+        int val = 5;
+        do {
+            val++;
+            System.out.println(val);
+        }while(val < 15);
+    }
 }
