b61c02d7b8f393bfce1bfce497020c7dc4da8b5c
Satwika <chekurusatwika@gmail.com>
2024-09-15 23:20:51 +0530

    implementatation of for loop

--- javaPrograming/day2conditionalStatements/task/ForLoop.java
+++ javaPrograming/day2conditionalStatements/task/ForLoop.java
@@ -1,5 +1,6 @@
 package com.javaPrograming.day2conditionalStatements.task;
 
+//implementation of for loop
 public class ForLoop {
     public static void main(String[] args) {
         for(int val = 6 ; val < 15 ; val++){
